e610b3fbe95293e0e21d799bdd3fb825331d6efd
MagB93 <magnus.leo93@gmail.com>
2017-09-05 19:17:53 +0200

    Updated oni config

--- .oni/config.js
+++ .oni/config.js
@@ -11,9 +11,9 @@ module.exports = {
    deactivate,
   //add custom config here, such as
   "oni.useDefaultConfig": false,
-  //"oni.bookmarks": ["~/Documents",]
   "statusbar.enabled": false,
   "oni.hideMenu": true,
+  "tabs.enabled": false,
   "tabs.showVimTabs": true,
   "editor.fontSize": "13px",
   "editor.fontFamily": "Source Code Pro"
